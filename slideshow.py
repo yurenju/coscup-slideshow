a3ac22fbd96d6949f5a5d8412976090de06403dd
Yuren Ju <yurenju@gmail.com>
2011-08-15 10:01:45 +0800

    let order correct

--- slideshow.py
+++ slideshow.py
@@ -12,10 +12,8 @@ class Slideshow:
         self.stage = clutter.Stage()
         self.stage.set_color(clutter.Color(0,0,0,255))
         self.files = sorted(os.listdir(image_dir))
-        print self.files
         if len(self.files) > maxium:
             self.files = self.files[maxium*-1:]
-        print self.files
         self.load_files()
         self.texture = None
         self.previous = None
@@ -44,13 +42,21 @@ class Slideshow:
             v.destroy()
 
     def append_files(self):
+        last = self.files[len(self.files)-1]
         files = sorted(os.listdir(image_dir), reverse=True)
-        if len(files) > 0 and files[0] != self.files[0]:
+
+        if len(files) > 0 and files[0] != last:
+            tmp = []
             for f in files:
-                if f != self.files[0]:
-                    self.files.insert(0, f)
+                if f != last:
+                    tmp.insert(0, f)
                 else:
                     break;
+            self.files = self.files + tmp
+            if len(self.files) > maxium:
+                self.files = self.files[maxium*-1:]
+            for f in tmp:
+                self.load_file(f)
             self.destroy_remain()
             return True
         else:
@@ -64,7 +70,7 @@ class Slideshow:
         scale = self.get_scale(texture)
         texture.set_scale(scale, scale)
         texture.set_opacity(0)
-        self.imageviews.insert(0, texture)
+        self.imageviews.append(texture)
     
     def load_files(self):
         self.imageviews = []
@@ -72,28 +78,30 @@ class Slideshow:
             self.load_file(f)
 
     def play(self, animation, pos=0):
-        if pos >= len(self.imageviews):
-            pos = 0
+        ret = self.append_files()
+        if pos < 0 or ret:
+            pos = len(self.files)-1
+
         self.pos = pos
         if self.texture != None:
             self.previous = self.texture
             self.previous.detach_animation()
             anim = self.previous.animate (clutter.LINEAR, interval, "opacity", 0)
-            anim.connect('completed', self.destroy_texture, self.previous)
+            anim.connect('completed', self.remove_texture, self.previous)
 
         print "total: %s, current: %s" % (len(self.files), pos)
         self.texture = self.imageviews[pos]
         self.stage.add(self.texture)
         anim = self.texture.animate (clutter.LINEAR, interval, "opacity", 255)
-        anim.connect('completed', self.play, pos+1)
+        anim.connect('completed', self.play, pos-1)
 
-    def destroy_texture(self, animation, actor):
+    def remove_texture(self, animation, actor):
         actor.get_parent().remove(actor)
 
 def main():
     slideshow = Slideshow()
     slideshow.load_files()
-    slideshow.play(None, pos=0)
+    slideshow.play(None, pos=len(slideshow.files)-1)
     clutter.main()
 
 if __name__ == '__main__':
